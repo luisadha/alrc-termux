381fdcf74fc079262947250c195c45c021e91633
luisadha <luisadha@noreply.codeberg.org>
2023-11-05 16:35:53 +0000

    Update alrc-termux.sh

--- alrc-termux.sh
+++ alrc-termux.sh
@@ -18,7 +18,7 @@
  # set -xv
 
 
-export ALRC_VERSION="4.0.11"
+export ALRC_VERSION="4.0.12"
 ALRC_UDATE='19/10/23 06:48:58 WIB'
 # export ALRC_HOME="$(cd -P -- "$(dirname -- "$(readlink "${BASH_SOURCE[0]}")")" && pwd)"
 export ALRC_HOME="$HOME/.local/share/alrc-termux"
