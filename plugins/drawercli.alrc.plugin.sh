22d1856749851e037e3ae06f9b3ca3d1ab123f85
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-03-18 06:10:59 +0700

    Update drawercli.alrc.plugin.sh

--- plugins/drawercli.alrc.plugin.sh
+++ plugins/drawercli.alrc.plugin.sh
@@ -1,6 +1,65 @@
 
-: "function _create_config() {
-  cat <<- "EOF" > $HOME/.drawercli_aliases
+#set -o errexit
+set -e
+i_install() {
+
+    echo -n "Installing ..."; echo;
+   
+}
+i_linkin() {
+   sleep 1; 
+   echo "Linking ..."
+ }
+i_setup() {
+   echo "Setup drawercli ..."
+}
+i_succ() {
+  echo "Successfull, drawercli package has been installed!"
+}
+
+i_found() {
+#echo "udah ada nih"
+return 1
+}
+
+main() {
+set -e
+  if ! grep -q 'alrc_plugin_enabled=(drawercli)' ~/.bashrc; then cat >> ~/.bashrc <<EOF
+
+alrc_plugin_enabled=(drawercli)
+EOF
+  else
+     i_found
+  fi
+  
+}
+main
+
+if [[ ! -x $(command -v alfetch 2> /dev/null) ]];
+  then
+   #i_install
+  if timeout 10s curl -fSsl "https://raw.githubusercontent.com/luisadha/drawercli/main/drawercli.sh" -o ~/.local/bin/drawercli 2> /dev/null && chmod +x ~/.local/bin/drawercli; 
+  then
+ i_install
+  else      echo -n "Failed. Timeout or network issue.";
+    
+  fi
+fi
+set +o noclobber
+sleep 1
+i_setup
+if ! grep -q 'termuxlauncher_config=true' ~/.bashrc; then
+cat >> ~/.bashrc <<EOF
+
+# Init termuxlauncher & drawercli aliases
+source $HOME/storage/shared/termuxlauncher/.apps-launcher 2> /dev/null 
+source $HOME/.drawercli_aliases
+termuxlauncher_config=true
+EOF
+fi
+
+  if [ ! -f ~/.drawercli_aliases ]; then
+cat <<- "EOF" > ~/.drawercli_aliases
 # Generate by plugin drawercli.plugin.alrc
 # require termuxlauncher app
 
@@ -10,82 +69,11 @@ alias app_recomendations='drawercli -S'
 alias app_refesh='drawercli -r'
 alias app_wallpaper='drawercli -w'
 EOF
-}
-
-function download_drawercli() {
-if [ ! -x $(command -v drawercli 2>/dev/null) ]; then
-  
-   echo -en "Downloading.. ";
-
-    if timeout 10s curl -fSsl "https://raw.githubusercontent.com/luisadha/drawercli/main/drawercli" -o ~/.local/bin/drawercli 2> /dev/null && chmod +x ~/.local/bin/drawercli; 
-    then sleep 1; echo -e " Done!"; onCreate;
-    else echo -e "Failed. Timeout or network issue."; return 1;
-    fi
 fi
+set +e
+i_linkin
+    ln -s ~/.local/bin/drawercli ~/.shortcuts/drawercli 2>/dev/null;
+  install ~/.shortcuts/drawercli ~/.shortcuts/drawercli.app 2>/dev/null;
+i_succ
 
-
-}
-
-  function ask() {
-
-  echo -e "${1} ${2}? [Y/n] " 
-  read SWITCH_CASE
-
-  case "$SWITCH_CASE" in
-
-    "" )
-      ${3}
-    ;;
-
-    y|Y )
-      ${3}
-    ;;
-
-    n|N )
-      echo "Abort."
-      return 1
-    ;;
-
-    * )
-      echo " Unknown '${SWITCH_CASE}'"
-      ask ${1} ${2} ${3}
-    ;;
-
-  esac
-
-}
-
-
-function onCreate() {
- if [ -f ~/.drawercli_aliases ]; then
-  source ~/.drawercli_aliases
-else
-  echo -en "Creating config files.. "
-_create_config
-source ~/.drawercli_aliases
-echo " Done!"; sleep 1; echo "You can find the configuration file at ~/.drawercli_aliases"
- fi
-
-}
-
-main() {
- local present=
-if type drawercli &> /dev/null; then
-  present=true
-  onCreate
-else 
-present=false;
-  ask "Are you sure to download" "the drawercli package?" download_drawercli 
- fi
-}
-echo "drawercli added"
-type drawercli
-main
-unset -f main
-unset -f ask
-unset -f onCreate
-unset -f download_drawercli
-unset -f _create_config 
-"
-echo "Under maintenance!"
 
