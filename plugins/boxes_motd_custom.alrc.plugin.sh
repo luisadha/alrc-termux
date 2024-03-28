833562cb029027dd84749b9673c4cad2c0464fa7
luisadha <adharudin14@gmail.com>
2024-03-29 01:14:47 +0700

    New plugin added, named boxes_motd_custom

--- plugins/boxes_motd_custom.alrc.plugin.sh
+++ plugins/boxes_motd_custom.alrc.plugin.sh
@@ -1,10 +1,22 @@
 #! bash alrc-termux.module
 
+if [ ! "$ALRC_MOTD_USE_BOXES" == "random" ]; then
+  alias al='setterm --cursor on; al | sort -i | boxes -d $ALRC_MOTD_USE_BOXES; set -o history;'
+else
+# Beritahu bahwa plugin ini telah diaktifkan dengan array
+plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
+alrc_plugin_enabled+=($plugin_shortname)
+readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
 
-alias al_login='source $ALRC_HOME/$ALRC_SOURCE; set -o history';
+# Periksa kondisi jika plugin ini aktif maka akan memodifikasi fungsi al ke alias al
+if [[ "${alrc_plugin_enabled[@]}" =~ "$plugin_shortname" ]]; then
 
+    alias al='al | sort -i | boxes -d $(al_shuf_boxes_design); set -o history; setterm --cursor on;';
+fi
+unset plugin_shortname
+fi
 #ping -c 1 -w 5 google.com &>/dev/null || echo -e "${warn}WARN:${reset}: This action requires an active ${italic}internet${reset} connection.\n"
-
+# Periksa apakah sudah ada file template border jika tidak maka mendownload automatis
 if [ ! -f $ALRC_HOME/cache/box-designs-templete.txt ]; then
 
 echo -e ${info}"INFO${reset}: Sedang mengunduh... add-on untuk fitur custom motd dengan border, dibutuhkan untuk package ${bold}boxes.${reset}"
