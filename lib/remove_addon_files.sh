abdc73b6384e3ca407283c0ded4072745b3b0403
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-03-30 11:33:23 +0700

    Remove readme.html

--- lib/remove_addon_files.sh
+++ lib/remove_addon_files.sh
@@ -3,6 +3,7 @@
 unset -f generate_addon_files
 rm -f ~/.local/bin/brandomusic-cache-clear.sh
 rm -f ~/.local/bin/check_ip_publics
+rm -f $ALRC_HOME/README.html
 echo -n "alrc-termux: removing addon files.."
 sleep 0.1;
 echo " Success!"
