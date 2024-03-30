b1d2ecc28120b326c99e31dedceb80be1c1c7ccf
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-03-30 11:26:53 +0700

    Naming plugin
    Decription Same as similiar files

--- plugins/brandomusicxforzsh.alrc.plugin.sh
+++ plugins/brandomusicxforzsh.alrc.plugin.sh
@@ -7,6 +7,8 @@ alias brandomusicxz='termux-media-player play "\$(realpath "\${ARG:-\$(busybox l
 brandomusicxz_present=true
 EOF
 fi
-alrc_plugin_enabled+=(brandomusicxforzsh)
+plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
+alrc_plugin_enabled+=($plugin_shortname)
 readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
+unset plugin_shortname
 echo "zshrc just now modified"
