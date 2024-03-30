11d2aabc952a2e9da596534a487d92b7c765001c
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-03-30 11:24:41 +0700

    Naming plugin

--- plugins/brandomusicv.alrc.plugin.sh
+++ plugins/brandomusicv.alrc.plugin.sh
@@ -65,5 +65,8 @@ EOF
     esac
 return
 }
-alrc_plugin_enabled+=(brandomusicv)
+
+plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
+alrc_plugin_enabled+=($plugin_shortname)
 readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
+unset plugin_shortname
