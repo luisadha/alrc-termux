fb64210d731a1c266ccaccbd299322cbf1514c56
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-06-08 15:59:53 +0700

    Update bash-preexec.alrc.plugin.sh
    Change text activating

--- plugins/bash-preexec.alrc.plugin.sh
+++ plugins/bash-preexec.alrc.plugin.sh
@@ -4,8 +4,10 @@ if [[ ! ${bash_preexec_imported:-${__bp_imported:-}} ]]; then
   plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
  ps=${plugin_shortname} alrc_plugin_enabled+=($plugin_shortname)
   readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
-echo -e "alrc-termux: plugin ${ps} enabled\n"
-echo -e "please edit your precmd or preexec at $ALRC_HOME/lib/ext_command_helper.sh"
+echo -e "alrc-termux: Plugin ${ps} successfully activated!\n"
+echo -e "Trying to check precmd & preexec contents if any... ";
+echo -e "${preexec_functions[@]}\n"
+echo -e "${precmd_functions[@]}"
   unset ps
   unset plugin_shortname
 fi
