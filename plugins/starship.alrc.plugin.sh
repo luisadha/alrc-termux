cad18e27a99743c4b5b7f2d0af4df49916458504
luisadha <adharudin14@gmail.com>
2024-07-08 22:40:46 +0700

    Resolve issue

--- plugins/starship.alrc.plugin.sh
+++ plugins/starship.alrc.plugin.sh
@@ -2,22 +2,23 @@
 
 if [[ -x $(which starship 2>/dev/null) ]]; then
   eval "$(starship init bash)"
-
+set +o noclobber
 plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
- ps=${plugin_shortname} alrc_plugin_enabled+=($plugin_shortname)
+ ps=${plugin_shortname};
+ alrc_plugin_enabled+=($plugin_shortname);
 
  readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
 
- echo -e "alrc-termux: Plugin ${ps} successfully loadded!\n."
+ echo -e "alrc-termux: Plugin ${ps} successfully loadded!\n"
 
- mkdir -p $ALRC_HOME/prompt/starship/
+ mkdir -p $ALRC_HOME/prompt/starship/ &>/dev/null
 
 for import in $(starship preset -l); do
 eval "starship preset $import > $ALRC_HOME/prompt/starship/${import}.toml"; 
 done
 
 if [ $ALRC_USE_STARSHIP == "random" ]; then
-  al_shuf_starship_prompt
+  al_shuf_starship_prompt &>/dev/null
 fi
   unset ps;
   unset plugin_shortname;
