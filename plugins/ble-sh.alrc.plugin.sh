5f639b30bac5955b27aa0130eb690cd5437f80e0
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-07-16 18:42:31 +0700

    Adding blesh plugin

--- plugins/ble-sh.alrc.plugin.sh
+++ plugins/ble-sh.alrc.plugin.sh
@@ -1,1 +1,8 @@
+if [[ "$HOME/.local/share/blesh/ble.sh" ]]; then
 source ~/.local/share/blesh/ble.sh
+plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' );
+ps=${plugin_shortname} alrc_plugin_enabled+=($plugin_shortname);
+readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
+echo -e "alrc-termux: Plugin ${ps} successfully loadded!\n" 
+unset ps; unset plugin_shortname;
+fi
