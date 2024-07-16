9084a5b6072cbb5af7e5d902e94aa015b700696f
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-07-16 18:45:41 +0700

    Update blesh.alrc.plugin.sh

--- plugins/blesh.alrc.plugin.sh
+++ plugins/blesh.alrc.plugin.sh
@@ -3,6 +3,6 @@ source ~/.local/share/blesh/ble.sh
 plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' );
 ps=${plugin_shortname} alrc_plugin_enabled+=($plugin_shortname);
 readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
-echo -e "alrc-termux: Plugin ${ps} successfully loadded!\n" 
+echo -e "alrc-termux: Plugin ${ps} successfully loadded!" 
 unset ps; unset plugin_shortname;
 fi
