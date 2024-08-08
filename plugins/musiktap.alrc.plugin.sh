d855ab5ec8969ded7974325e237974ba4cd85fc0
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-08-08 22:58:51 +0700

    Improvement module musiktap

--- plugins/musiktap.alrc.plugin.sh
+++ plugins/musiktap.alrc.plugin.sh
@@ -1,12 +1,6 @@
 #! bash alrc-termux.module
-plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
-alrc_plugin_enabled+=($plugin_shortname)
-readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
-
 function main() {
-local ps=${plugin_shortname}
 cat <<- "EOF" > $HOME/.shortcuts/musiktap.app
-#!/data/data/com.termux/files/usr/bin/bash
 
 # Cli-apps for termux widget "Musiktap adalah konfigurasi untuk menjalankan library musik.sh"
 # Load module from alrc-termux
@@ -14,20 +8,23 @@ cat <<- "EOF" > $HOME/.shortcuts/musiktap.app
 #by luisadha 
 #
 
-export IMG_COVER="$HOME/.local/share/alrc-termux/img/miku_75949.png" # path to your image for ascii generating
-#export COLOR_GRADIENT="magma" # uncomment to random generate gradient colors
+export IMG_COVER="${ALRC_HOME:-~/.alrc.sandbox}/img/miku_75949.png" # path to your image for ascii generating
+export COLOR_GRADIENT="magma" # uncomment to random generate gradient colors
 export ANIMATE_DURATION=5 # Durasi animasi pewarna diset ke nol secara default.
 export DATE="static" # static yaitu menampilkan jam dan menit saat terakhir program dijalankan
 # Unkomentar untuk generate ke dinamis mengikuti menit saat ini /realtime.
 export MAX_COLUMN=296
 
-bash --init-file ~/.local/share/alrc-termux/lib/music.sh
+bash --init-file ${ALRC_HOME:-~/.alrc.sandbox}/lib/music.sh
 # Tidak pakai source karena pasti bakal error
 
 EOF
-echo -e "alrc-termux: Plugin ${ps} successfully loadded!"
-echo "+1 \"${ps}.app\" file added at ~/.shortcuts"
 }
+plugin_shortname=$(echo "${BASH_SOURCE[0]}" | awk '{gsub(/.*[/]|[.].*/, "", $0)} 1' )
+alrc_plugin_enabled+=($plugin_shortname)
+readarray -t alrc_plugin_enabled <<< $(printf "%s\n" "${alrc_plugin_enabled[@]}" | sort -u)
+
+echo "1 files Added at ~/.shortcuts."
 main;
 unset -f main;
 unset plugin_shortname
